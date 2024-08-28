b4dfc96149cf7de5080cc91a8bd948b25cae5a63
traintagger <99@mba.local>
2024-08-27 21:46:42 -0500

    fix

--- 3. Multiplicacion por bloques/build/CMakeFiles/matrixTest02.dir/src/header.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/matrixTest02.dir/src/header.cpp.o: \
-  /Users/99/202402/Computacion\ Paralela\ y\ Distribuida/Pruebas\ sobre\ el\ comportamiento\ de\ la\ memoria\ caché/03/src/header.cpp \
-  /Users/99/202402/Computacion\ Paralela\ y\ Distribuida/Pruebas\ sobre\ el\ comportamiento\ de\ la\ memoria\ caché/03/src/../include/header.hpp
